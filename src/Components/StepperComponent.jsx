7c873903035b3fc6fa6390274f524e470c6cdc9e
abhijeet4111 <abhijeet.ekad@xecurify.com>
2023-07-16 00:19:02 +0530

    initial commit

---
+++ src/Components/StepperComponent.jsx
@@ -0,0 +1,169 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepButton from "@mui/material/StepButton";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useDispatch, useSelector } from "react-redux";
+import { activeTab } from "../Utils.jsx/TabSlice";
+import { AddCapabilities } from "./AddCapabilities";
+import { AddRoleName } from "./AddRoleName";
+import { Preview } from "./Preview";
+const steps = ["Add Role Name", "Add Capabilities ", "Preview"];
+const StepperComponent = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const [completed, setCompleted] = useState({});
+  const disPatch = useDispatch();
+
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+          // find the first step that has been completed
+          steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+
+  const handleComplete = () => {
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    handleNext();
+    disPatch(activeTab(steps[activeStep]));
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+
+    setCompleted({});
+  };
+  disPatch(activeTab(steps[activeStep]));
+  console.log(completed);
+  return (
+    <Box sx={{ width: "100%", padding: "2rem" }}>
+      <Stepper nonLinear activeStep={activeStep}>
+        {steps.map((label, index) => (
+          <Step key={label} completed={completed[index]}>
+            <StepButton color="inherit" onClick={handleStep(index)}>
+              {label}
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {allStepsCompleted() ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Box sx={{ flex: "1 1 auto" }} />
+              <Button variant="contained" onClick={handleReset}>
+                Reset
+              </Button>
+            </Box>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+              Step {activeStep + 1}
+            </Typography>
+            {activeStep === 0 && <AddRoleName />}
+            {activeStep === 1 && <AddCapabilities />}
+            {activeStep === 2 && <Preview />}
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              {/* <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button> */}
+              <Button color="inherit" onClick={handleNext} sx={{ mr: 1 }}>
+                Next
+              </Button>
+              <Box sx={{ flex: "1 1 auto" }} />
+              {/* <Button variant="contained" onClick={handleNext} sx={{ mr: 1 }}>
+                Next
+              </Button> */}
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+              {activeStep === 0 ? (
+                <div className="activeStep0Btn">
+                  {activeStep !== steps.length &&
+                    (completed[activeStep] ? (
+                      <Typography
+                        variant="caption"
+                        sx={{ display: "inline-block" }}
+                      >
+                        Step {activeStep + 1} already completed
+                      </Typography>
+                    ) : (
+                      <Button variant="contained" onClick={handleComplete}>
+                        {completedSteps() === totalSteps() - 1
+                          ? "Finish"
+                          : "Save & Next"}
+                      </Button>
+                    ))}
+                </div>
+              ) : (
+                <div className="activeStepOtherBtn">
+                  {activeStep !== steps.length &&
+                    (completed[activeStep] ? (
+                      <Typography
+                        variant="caption"
+                        sx={{ display: "inline-block" }}
+                      >
+                        Step {activeStep + 1} already completed
+                      </Typography>
+                    ) : (
+                      <Button variant="contained" onClick={handleComplete}>
+                        {completedSteps() === totalSteps() - 1
+                          ? "Finish"
+                          : "Save & Next"}
+                      </Button>
+                    ))}
+                </div>
+              )}
+            </Box>
+          </React.Fragment>
+        )}
+      </div>
+    </Box>
+  );
+};
+export { StepperComponent };
